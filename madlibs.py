7f3b0139c54714aff0df87992f6811dc283bb752
Joao Gabriel <jgafarias1@gmail.com>
2025-02-02 12:28:37 -0300

    First Commit
    Added madlibs.py:
    Defined variables with input and f-string
    Called madlib (f-string) with print.

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Beginner Python Project
+
+# Variables with inputs
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+
+# F-String
+madlib = f'Computer programmig is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}.'
+
+# Output
+print(madlib)
